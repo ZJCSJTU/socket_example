122ac746a9d9f4f641f8e43ef905d61c7cc111da
David Devecsery <ddevec@umich.edu>
2014-11-07 00:03:51 -0500

    Moar cleaning

--- client.cpp
+++ client.cpp
@@ -15,14 +15,13 @@ using namespace std;
 static const char * data = "Hello World";
 
 /*
-* This program creates a socket and initiates a connection with the
-* socket given in the command line. Some data are sent over the
-* connection and then the socket is closed, ending the connection.
-* The form of the command line is: client hostname portnumber
-* Usage: pgm host port
-*/
-int main(int argc, char *argv[])
-{
+ * This program creates a socket and initiates a connection with the
+ * socket given in the command line. Some data are sent over the
+ * connection and then the socket is closed, ending the connection.
+ * The form of the command line is: client hostname portnumber
+ * Usage: pgm host port
+ */
+int main(int argc, char *argv[]) {
 	int sock;
 	struct sockaddr_in server;
 	struct hostent *hp;
@@ -34,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 	
 	/* Create socket. */
-	sock = socket( AF_INET, SOCK_STREAM, 0 );
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1) {
 		cerr << "opening stream socket: " << strerror(errno) << endl;
 		exit(EXIT_FAILURE);
@@ -45,19 +44,23 @@ int main(int argc, char *argv[])
 	hp = gethostbyname(argv[1]);
 	
 	/*
-	* gethostbyname returns a structure including the network address
-	* of the specified host.
-	*/
+	 * gethostbyname returns a structure including the network address
+	 * of the specified host.
+	 */
 	if (hp == nullptr) {
 		cerr << argv[1] << ": unknown host" << endl;
 		exit(EXIT_FAILURE);
 	}
 
-	memcpy(reinterpret_cast<char *>(&server.sin_addr), reinterpret_cast<char *>(hp->h_addr), hp->h_length);
+	memcpy(reinterpret_cast<char *>(&server.sin_addr),
+			reinterpret_cast<char *>(hp->h_addr), hp->h_length);
 
+	/* Set the client to connect to the given server port */
 	server.sin_port = htons(atoi(argv[2]));
 
-	rval = connect(sock, reinterpret_cast<struct sockaddr *>(&server), sizeof(server));
+	/* Actually connect to the desired server port */
+	rval = connect(sock, reinterpret_cast<struct sockaddr *>(&server),
+			sizeof(server));
 	if (rval == -1) {
 		cerr << "connecting stream socket: " << strerror(errno) << endl;
 		exit(EXIT_FAILURE);
@@ -66,7 +69,13 @@ int main(int argc, char *argv[])
 	int nsent = 0;
 	int ntosend = strlen(data)+1;
 	const char *data_ptr = data;
+	/* 
+	 * We have to send in a loop, because send doesn't guarantee that it will
+	 * successfully send all the characters we want to!
+	 */
 	while (ntosend != 0) {
+
+		/* Send any unsent data bytes remaining */
 		rval = send(sock, data_ptr + nsent, ntosend, 0);
 		if (rval == -1) {
 			cerr << "sending on stream socket: " << strerror(errno) << endl;
--- server.cpp
+++ server.cpp
@@ -13,8 +13,7 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
 	int sock;
 	struct sockaddr_in server;
 	int msgsock;
@@ -30,9 +29,14 @@ int main()
 	}
 	
 	/* Bind socket using wildcards.*/
+	/* This is an internet socket */
 	server.sin_family = AF_INET;
+	/* Accepts any in address */
 	server.sin_addr.s_addr = INADDR_ANY;
+	/* Allow the system to specify a port for us */
 	server.sin_port = 0;
+
+	/* Bind the socket to our given address space */
 	rval = bind(sock, reinterpret_cast<sockaddr *>(&server), sizeof(server));
 	if (rval == -1) {
 		cerr << "binding stream socket " << strerror(errno) << endl;
@@ -41,12 +45,17 @@ int main()
 	
 	/* Find out assigned port number and print it out. */
 	socklen_t length = sizeof(server);
+	/* Get the info about how this socket was bound */
 	rval = getsockname(sock, reinterpret_cast<sockaddr *>(&server), &length);
 	if (rval == -1) {
 		cerr << "getting socket name " << strerror(errno) << endl;
 		exit(EXIT_FAILURE);
 	}
 	
+	/* 
+	 * Convert the network style port number from addr to a architecture
+	 * compatible port number with ntohs (network to host short)
+	 */
 	int server_port = ntohs(server.sin_port);
 	cout << "Socket port #" << server_port << endl;
 	
@@ -57,6 +66,7 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	/* Actually get a connection, this returns a new fd for our new connection */
 	msgsock = accept(sock,(struct sockaddr *) 0,(socklen_t *) 0);
 	if (msgsock == -1) {
 		cerr << "accept: " << strerror(errno) << endl;
@@ -64,11 +74,14 @@ int main()
 	}
 	
 	do {
-		// Zero our buffer
+		/* Zero our buffer */
 		buf.fill(0);
 
-		if ((rval = recv(msgsock, static_cast<void *>(buf.data()), buf.size(), 0)) == -1) {
+		/* Read the bytes off the network socket */
+		rval = recv(msgsock, static_cast<void *>(buf.data()), buf.size(), MSG_NOSIGNAL);
+		if (rval == -1) {
 			cerr << "reading stream message: " << strerror(errno) << endl;
+			exit(EXIT_FAILURE);
 		}
 
 		if (rval == 0) {
@@ -80,6 +93,6 @@ int main()
 
 	close(msgsock);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
